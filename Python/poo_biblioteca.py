6cd9d941c09dccdb451cf27ccc4b4dec8e173fb4
Humberto Alcantara <humbertojr_pe@hotmail.com>
2024-08-14 21:59:38 -0300

    Ajuste

--- Python/poo_biblioteca.py
+++ Python/poo_biblioteca.py
@@ -4,6 +4,7 @@ class Biblioteca:
     def __init__(self, nome_biblioteca):
         self.nome_biblioteca = nome_biblioteca
         self.livros = []
+        self.livros_alugados = []
     
     def adicionar_livro(self, livro):
         self.livros.append(livro)
@@ -16,7 +17,6 @@ class Biblioteca:
         self.adicionar_livro(novo_livro)
 
     def exibir_livros(self):
-        # Exibir todos os livros cadastrados com todas as informações
         for livro in self.mostrar_livros():
             print(f"Título: {livro[0]}")
             print(f"Autor: {livro[1]}")
@@ -28,6 +28,24 @@ class Biblioteca:
         nomes_livros = ', '.join([livro[0] for livro in self.mostrar_livros()])
         return f'Biblioteca: {self.nome_biblioteca}\nLivros: {nomes_livros}'
 
+    def alugar_livro(self, titulo):
+        for livro in self.livros:
+            if livro.titulo == titulo:
+                self.livros.remove(livro)
+                self.livros_alugados.append(livro)
+                print(f'Você alugou o livro "{titulo}".')
+                return
+        print(f'O livro "{titulo}" não está disponível para aluguel.')
+
+    def devolver_livro(self, titulo):
+        for livro in self.livros_alugados:
+            if livro.titulo == titulo:
+                self.livros_alugados.remove(livro)
+                self.adicionar_livro(livro)
+                print(f'Você devolveu o livro "{titulo}".')
+                return
+        print(f'O livro "{titulo}" não foi encontrado entre os livros alugados.')
+
 def main():
     biblioteca = Biblioteca('Biblioteca Conhecimento Nunca é D+')
 
@@ -37,37 +55,42 @@ def main():
         print('''
         1 - Cadastrar Livro
         2 - Mostrar Livros
-        3 - SAIR
+        3 - Alugar Livro
+        4 - Devolver Livro
+        5 - SAIR
         ''')
         print('-='*30)
         res = int(input('Digite uma Opção: '))
         
         if res == 1:
-            # Pedir ao usuário para inserir os detalhes do livro
             titulo = input('Digite o título do livro: ')
             autor = input('Digite o autor do livro: ')
             ano = int(input('Digite o ano de publicação: '))
             editora = input('Digite a editora do livro (ou pressione Enter para "Não informado"): ')
-            
-            # Se o usuário não fornecer uma editora, usar "Não informado"
             if not editora:
                 editora = 'Não informado'
-            
             biblioteca.cadastrar_livro(titulo, autor, ano, editora)
             print('Livro cadastrado com sucesso!')
         
         elif res == 2:
             print('Mostrando todos os livros cadastrados:')
             print(biblioteca.resumo())
-            print('-='*30)
             biblioteca.exibir_livros()
-        
+
         elif res == 3:
+            titulo = input('Digite o título do livro que deseja alugar: ')
+            biblioteca.alugar_livro(titulo)
+        
+        elif res == 4:
+            titulo = input('Digite o título do livro que deseja devolver: ')
+            biblioteca.devolver_livro(titulo)
+        
+        elif res == 5:
             print('Saindo do programa')
             break
         
         else:
             print('Opção inválida. Tente novamente.')
 
-
 main()
+
