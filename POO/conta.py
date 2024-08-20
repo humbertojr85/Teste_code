f3b46dcc27c23d5f9a2c2e2bc75889c445be13d6
Humberto Alcantara <humbertojr_pe@hotmail.com>
2024-08-19 23:08:35 -0300

    Funcao senha

--- POO/conta.py
+++ POO/conta.py
@@ -1,10 +1,15 @@
 class Conta:
-    def __init__(self, titular, saldo):
+    def __init__(self, titular, saldo, senha=123):
         self.titular = titular
         self.saldo = saldo
+        self.senha = senha
 
     def extrato(self):
-        print(f'Titular: {self.titular} \nSaldo: {self.saldo}')
+        sen = int(input('Digite a senha para ver o Saldo: '))
+        if sen == self.senha:
+            print(f'Titular: {self.titular} \nSaldo: {self.saldo}')
+        else:
+            print('Senha incorreta!')
 
     def depositar(self, deposito):
         deposito = float(input(f'Qual o valor do Deposito: '))
